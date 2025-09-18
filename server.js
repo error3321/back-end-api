6baf030f1b80eb5de9a6e61411b102f0a2803527
error3321 <cristhianrfernandes@gmail.com>
2025-09-18 19:29:43 +0000

    m

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
+import pkg from "pg";
+import dotenv from "dotenv";
 import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
-app.get("/", (req, res) => {        // Cria endpoint na rota da raiz do projeto
+app.get("/", async (req, res) => {        // Cria endpoint na rota da raiz do projeto
   console.log("Rota GET / solicitada");
   res.json({
 		message: "API para futebol",      // Substitua pelo conteúdo da sua API
     author: "Cristhian Rangel Fernandes",    // Substitua pelo seu nome
+    statusBD: dbStatus
   });
 });
+
+dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
+
+const db = new Pool({  
+  connectionString: process.env.URL_BD,
+});
+
+let dbStatus = "ok";
+try {
+  await db.query("SELECT 1");
+} catch (e) {
+  dbStatus = e.message;
+}
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço rodando na porta:  ${port}`);
