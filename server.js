bafb6e4cf7bd52e47c2a113fb5c1603378c828f4
error3321 <cristhianrfernandes@gmail.com>
2025-09-25 19:09:58 +0000

    m

--- server.js
+++ server.js
@@ -6,18 +6,8 @@ import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
-app.get("/questoes", async (req, res) => {        // Cria endpoint na rota da raiz do projeto
-  try {
-    const resultado = await db.query("SELECT * FROM questoes"); // Executa uma consulta SQL para selecionar todas as questões
-    const dados = resultado.rows; // Obtém as linhas retornadas pela consulta
-    res.json(dados); // Retorna o resultado da consulta como JSON
-  } catch (e) {
-    console.error("Erro ao buscar questões:", e); // Log do erro no servidor
-    res.status(500).json({
-      erro: "Erro interno do servidor",
-      mensagem: "Não foi possível buscar as questões",
-    });
-  }
+app.get("/", async (req, res) => {        // Cria endpoint na rota da raiz do projeto
+  
   console.log("Rota GET /questoes solicitada");
   const db = new Pool({  
   connectionString: process.env.URL_BD,
@@ -41,4 +31,30 @@ res.json({
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço rodando na porta:  ${port}`);
+});
+
+//server.js
+app.get("/questoes", async (req, res) => {
+	console.log("Rota GET /questoes solicitada"); // Log no terminal para indicar que a rota foi acessada
+	
+  //server.js
+const { Pool } = pkg; // Obtém o construtor Pool do pacote pg para gerenciar conexões com o banco de dados PostgreSQL
+
+const db = new Pool({
+  // Cria uma nova instância do Pool para gerenciar conexões com o banco de dados
+  connectionString: process.env.URL_BD, // Usa a variável de ambiente do arquivo .env DATABASE_URL para a string de conexão
+});
+
+//server.js
+try {
+    const resultado = await db.query("SELECT * FROM questoes"); // Executa uma consulta SQL para selecionar todas as questões
+    const dados = resultado.rows; // Obtém as linhas retornadas pela consulta
+    res.json(dados); // Retorna o resultado da consulta como JSON
+  } catch (e) {
+    console.error("Erro ao buscar questões:", e); // Log do erro no servidor
+    res.status(500).json({
+      erro: "Erro interno do servidor",
+      mensagem: "Não foi possível buscar as questões",
+    });
+  }
 });
